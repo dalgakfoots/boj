9b69d071948e534a3362f2547e7d5fc00be6fa7b
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2022-07-15 15:22:58 +0900

    백준 10808 알파벳 개수

---
+++ src/q10808/Main.java
@@ -0,0 +1,28 @@
+package q10808;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) % 97;
+            arr[c] = arr[c] + 1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
