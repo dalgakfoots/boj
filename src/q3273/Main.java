6b90d07e6c30a8cee21020fc31d559f7bc219970
dalgakfoots <77476939+dalgakfoots@users.noreply.github.com>
2022-07-15 17:55:11 +0900

    백준 3273 두 수의 합

---
+++ src/q3273/Main.java
@@ -0,0 +1,26 @@
+package q3273;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] strArrN = br.readLine().split(" ");
+        int[] intArr = new int[n];
+        for(int i =0;i<n;i++) intArr[i] = Integer.parseInt(strArrN[i]);
+
+        int x = Integer.parseInt(br.readLine());
+
+        int index=0;
+        Arrays.sort(intArr); // 이진 검색 사용을 위한 정렬
+        for(int i=0;i<n;i++) index+=Arrays.binarySearch(intArr,(x-intArr[i]))>=0 ? 1:0;
+        System.out.println(index/2);
+    }
+}
